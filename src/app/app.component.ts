656cc176b4640eb397e17dc2ed46056b3403c4be
Rahul <rahulkm561@gmail.com>
2023-04-23 16:06:37 +0530

    push code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import SafeAppsSDK from '@safe-global/safe-apps-sdk';
+import { ethers } from 'ethers';
+import { catchError, combineLatest, forkJoin, Observable, of, switchMap, take, tap } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { EthereumService } from './services/ethereum.service';
+import { GnosisService } from './services/gnosis.service';
 
-type Opts = {
-  allowedDomains?: RegExp[];
-  debug?: boolean;
-};
-
-const opts: Opts = {
-  allowedDomains: [/gnosis-safe.io$/, /app.safe.global$/],
-  debug: false,
-};
-
-const appsSdk = new SafeAppsSDK(opts);
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -20,17 +13,77 @@ const appsSdk = new SafeAppsSDK(opts);
 export class AppComponent implements OnInit {
   title = 'safe-app';
 
-  constructor() {}
+  constructor(
+    private ethereumService: EthereumService,
+    private gnosisService: GnosisService,
+  ) {}
 
   ngOnInit(): void {
-    setTimeout(() => {
-      this.addListeners(); 
-    }, 1000);
   }
 
-  public async addListeners() {
-    const safe = await appsSdk.safe.getInfo();
-    console.log('safe', safe)
-  }
+  public swap(): void {
+    console.log('swap=>',227);
+  
+    const walletAddress$ = this.gnosisService.walletAddress$;
+
+    const transactions: any[] = [];
+    let token: any;
+    let walletAddress: string;
+    combineLatest([walletAddress$]).pipe(
+        switchMap(([addr]) => {
+            console.log('swap=>',238, addr);
+            walletAddress = addr;
+            token = addr;
+            console.log('swap=>',token);
+
+            const toToken = '0x427837FC0095b29BeA77e175A10bAa852A29DAe5';
+            console.log('swap=>',toToken);
+            const price:any  = '1.0';                        
+            const isTokenApproved$ = this.ethereumService.isTokenApproved(
+                token,
+                walletAddress,
+                environment.TOKEN_SPENDER,
+                price
+            );
+            return forkJoin({
+                isApproved: isTokenApproved$,
+                fromToken: of(token),
+                toToken: of(toToken)
+            });
+            
+        }),
+        switchMap(({isApproved, fromToken, toToken}) => {
+            console.log('swap=>',253, isApproved, fromToken, toToken);
+            if (!isApproved) {
+                const tx: any = {
+                    to: token,
+                    data: this.ethereumService.getApproveCallData(environment.TOKEN_SPENDER, ethers.constants.MaxUint256),
+                    value: '0'
+                };
+                transactions.push(tx);
+            }
+
+            return new Observable()
+        }),
+        tap((data: any) => {
+            console.log('swap=>',273, data);
+
+            const tx: any = {
+                to: data.tx.to,
+                value: data.tx.value,
+                data: data.tx.data,
+                gasPrice: data.tx.gasPrice
+            };
+            transactions.push(tx);
+            this.gnosisService.sendTransactions(transactions);
+        }),
+        catchError((e) => {
+            console.log(e);
+            return of('');
+        }),
+        take(1),
+    ).subscribe();
+}
+
 
 }
---
+++ src/app/services/gnosis.service.ts
@@ -0,0 +1,53 @@
+import SafeAppsSDK from '@safe-global/safe-apps-sdk';
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+import { shareReplay } from 'rxjs/operators';
+
+type Opts = {
+    allowedDomains?: RegExp[];
+    debug?: boolean;
+  };
+  
+  const opts: Opts = {
+    allowedDomains: [/gnosis-safe.io$/, /app.safe.global$/],
+    debug: false,
+  };
+  
+const appsSdk = new SafeAppsSDK(opts);
+export type Tx = {
+    to: string;
+    value: string;
+    data: string;
+    gasPrice?: string;
+    gas?: string;
+};
+
+@Injectable({
+    providedIn: 'root'
+})
+export class GnosisService {
+
+    private walletAddress = new Subject<string>();
+    public walletAddress$ = this.walletAddress.pipe(
+        shareReplay({ bufferSize: 1, refCount: true })
+    );
+
+    // todo: add alert for testnet (not supported)
+    private isMainNet = new Subject<boolean>();
+    public isMainNet$ = this.isMainNet.pipe(
+        shareReplay({ bufferSize: 1, refCount: true })
+    );
+
+    constructor() {
+    }
+
+    public sendTransactions(txs: Tx[]): void {
+        //appsSdk.sendTransactions(txs);
+    }
+
+    public async addListeners() {
+        const safe = await appsSdk.safe.getInfo();
+        this.isMainNet.next(true);
+        this.walletAddress.next(safe.safeAddress);
+    }
+}
