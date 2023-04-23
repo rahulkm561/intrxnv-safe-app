74d7acbb8fd2494068e4d6853fc2bbd15cfbf81a
Rahul <rahulkm561@gmail.com>
2023-04-23 15:20:50 +0530

    push code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import initSdk, { SafeInfo } from '@gnosis.pm/safe-apps-sdk';
+
+const appsSdk = initSdk();
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'safe-app';
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.addListeners();
+  }
+
+  public addListeners(): void {
+    appsSdk.addListeners({
+      onSafeInfo: (info: SafeInfo) => {
+        console.log('info', info);
+      },
+    });
+  }
+
+  public removeListeners(): void {
+    appsSdk.removeListeners();
+  }
 }
